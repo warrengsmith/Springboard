4cba277bd2e771b78ebfee7d27f41b7c2c9fc16c
Warren <Warren@Warren-PC>
2014-05-27 21:32:33 -0400

    Clean up initial HomeController

--- wgs-virago/src/main/java/com/wgs/virago/HomeController.java
+++ wgs-virago/src/main/java/com/wgs/virago/HomeController.java
@@ -1,16 +1,6 @@
 package com.wgs.virago;
 
-import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,15 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.usg.ssg1.common.dao.BookDAO;
 import com.usg.ssg1.common.dto.Book;
-import com.wgs.virago.dao.JdbcTemplateBookDAOImpl;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private BookDAO bookDAO;
@@ -36,22 +23,9 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Model model) {
 		List<Book> bookList = this.bookDAO.findAllBooks();
-		
-		
 		model.addAttribute("bookList", bookList);
-		
 		return "home";
 	}
-	
 }
