19219a1f78719a165f50c0974d908d6c816143a4
warren.smith <wsmith@kemper.com>
2014-05-27 20:54:50 -0400

    Security initial commit

---
+++ wgs-virago/src/main/java/com/wgs/virago/security/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.wgs.virago.security;
+
+import org.springframework.security.web.context.*;
+
+public class MessageSecurityWebApplicationInitializer {// extends AbstractSecurityWebApplicationInitializer {
+}
