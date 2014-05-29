8c0723bbfa9335c55b53f129fba6fb1752c08110
Warren <Warren@Warren-PC>
2014-05-28 23:54:40 -0400

    Adding Javadoc to HomeController

--- wgs-virago/src/main/java/com/wgs/virago/HomeController.java
+++ wgs-virago/src/main/java/com/wgs/virago/HomeController.java
@@ -15,15 +15,21 @@ import com.usg.ssg1.common.dto.Book;
 
 /**
  * Handles requests for the application home page.
+ * @author Warren Smith
+ * @version 1.0
  */
 @Controller
 public class HomeController {
 	
+	/** The book dao. */
 	@Autowired
 	private BookDAO bookDAO;
 		
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 *
+	 * @param model the model
+	 * @return the string
 	 */
 	@RequestMapping(value = "/allBooks", method = RequestMethod.GET)
 	public String home(Model model) {
@@ -33,6 +39,13 @@ public class HomeController {
 		return "bookList";
 	}
 	
+	/**
+	 * Method Book search.
+	 *
+	 * @param bookPart the book part
+	 * @param model the model
+	 * @return the string
+	 */
 	@RequestMapping(value = "/bookSearch", method = RequestMethod.GET)
 	public String bookSearch(@RequestParam("bookArg") String bookPart, Model model) {
 		List<Book> bookList = this.bookDAO.searchByTitle(bookPart);
@@ -41,12 +54,25 @@ public class HomeController {
 		return "bookList";
 	}
 	
+	/**
+	 * Method Search form.
+	 *
+	 * @param model the model
+	 * @return the string
+	 */
 	@RequestMapping(value="/search", method=RequestMethod.GET)
     public String searchForm(Model model) {
         model.addAttribute("book", new Book());
         return "search";
     }
 	
+	/**
+	 * Method Search submit.
+	 *
+	 * @param book the book
+	 * @param model the model
+	 * @return the string
+	 */
 	@RequestMapping(value = "/search", method = RequestMethod.POST)
 	public String searchSubmit(@ModelAttribute Book book, Model model) {
 		List<Book> bookList = this.bookDAO.searchByTitle(book.getTitle());
