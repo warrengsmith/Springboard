3942bc5beae4c8f9ec1a4714f1921d7f0baea930
Warren <Warren@Warren-PC>
2014-06-03 00:11:45 -0400

    Security - step 1...updated the POM, now updated web.xml

--- wgs-virago/src/main/java/com/wgs/virago/security/MessageSecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.wgs.virago.security;
-
-import org.springframework.security.web.context.*;
-
-public class MessageSecurityWebApplicationInitializer {// extends AbstractSecurityWebApplicationInitializer {
-}
