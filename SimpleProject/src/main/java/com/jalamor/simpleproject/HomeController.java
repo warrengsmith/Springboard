2a2205167a47b6f6b0d7acf64ae766664876a68e
warren.smith <wsmith@kemper.com>
2014-06-11 08:50:14 -0400

    Tidy up HomeController in SimpleProject

--- SimpleProject/src/main/java/com/jalamor/simpleproject/HomeController.java
+++ SimpleProject/src/main/java/com/jalamor/simpleproject/HomeController.java
@@ -1,14 +1,9 @@
 package com.jalamor.simpleproject;
 
 import java.math.BigDecimal;
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,22 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Model model) {
+		List<Book> bookList = getBookList();
+		model.addAttribute("bookList", bookList);
+		return "home";
+	}
+
+	private List<Book> getBookList() {
 		List<Book> bookList = new ArrayList<Book>();
 		Book book1 = new Book();
 		book1.setId(1);
@@ -121,9 +111,90 @@ public class HomeController {
 		book12.setPrice(new BigDecimal("11.59"));
 		bookList.add(book12);
 		
-		model.addAttribute("bookList", bookList);
+		Book book13 = new Book();
+		book13.setId(13);
+		book13.setTitle("Paris");
+		book13.setAuthor("Edward Rutherfurd");
+		book13.setPrice(new BigDecimal("14.49"));
+		bookList.add(book13);
 		
-		return "home";
+		Book book14 = new Book();
+		book14.setId(14);
+		book14.setTitle("Orphan Train");
+		book14.setAuthor("Christina Baker Kline");
+		book14.setPrice(new BigDecimal("6.99"));
+		bookList.add(book14);
+		
+		Book book15 = new Book();
+		book15.setId(15);
+		book15.setTitle("Remember me like this");
+		book15.setAuthor("Bret Anthony Johnston");
+		book15.setPrice(new BigDecimal("10.99"));
+		bookList.add(book15);
+			
+		Book book16 = new Book();
+		book16.setId(16);
+		book16.setTitle("The Kitchen House");
+		book16.setAuthor("");
+		book16.setPrice(new BigDecimal("8.62"));
+		bookList.add(book16);
+		
+		Book book17 = new Book();
+		book17.setId(17);
+		book17.setTitle("Finding Pride");
+		book17.setAuthor("Jill Sanders");
+		book17.setPrice(new BigDecimal("11.59"));
+		bookList.add(book17);
+		
+		Book book18 = new Book();
+		book18.setId(18);
+		book18.setTitle("Paris");
+		book18.setAuthor("Edward Rutherfurd");
+		book18.setPrice(new BigDecimal("14.49"));
+		bookList.add(book18);
+		
+		Book book19 = new Book();
+		book8.setId(19);
+		book19.setTitle("Orphan Train");
+		book19.setAuthor("Christina Baker Kline");
+		book19.setPrice(new BigDecimal("6.99"));
+		bookList.add(book19);
+		
+		Book book20 = new Book();
+		book20.setId(20);
+		book20.setTitle("Remember me like this");
+		book20.setAuthor("Bret Anthony Johnston");
+		book20.setPrice(new BigDecimal("10.99"));
+		bookList.add(book20);
+		
+		Book book21 = new Book();
+		book21.setId(21);
+		book21.setTitle("The Kitchen House");
+		book21.setAuthor("Kathleen Grissom");
+		book21.setPrice(new BigDecimal("8.62"));
+		bookList.add(book21);
+		
+		Book book22 = new Book();
+		book22.setId(22);
+		book22.setTitle("The Kitchen House");
+		book22.setAuthor("");
+		book22.setPrice(new BigDecimal("8.62"));
+		bookList.add(book22);
+		
+		Book book23 = new Book();
+		book23.setId(23);
+		book23.setTitle("Finding Pride");
+		book23.setAuthor("Jill Sanders");
+		book23.setPrice(new BigDecimal("11.59"));
+		bookList.add(book23);
+		
+		Book book24 = new Book();
+		book24.setId(24);
+		book24.setTitle("The Kitchen House");
+		book24.setAuthor("Kathleen Grissom");
+		book24.setPrice(new BigDecimal("8.62"));
+		bookList.add(book24);
+		return bookList;
 	}
 	
 }
